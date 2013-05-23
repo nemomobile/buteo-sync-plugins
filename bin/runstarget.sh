7e860c1c5149e87efd937864e49eced479a0db8f
Martin Kampas <martin.kampas@tieto.com>
2013-05-23 05:02:20 +0000

    [tests] Remove unused environment settings
    Relates NEMO#663

--- bin/runstarget.sh
+++ bin/runstarget.sh
@@ -3,11 +3,5 @@
 echo "running ${1}..."
 
 cd /opt/tests/buteo-sync-plugins
-if [ -f /tmp/session_bus_address.user ]; 
-then 
-source /tmp/session_bus_address.user;
-export DISPLAY=:1
-fi	
-export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
 
 exec ${1} -maxwarnings 0
