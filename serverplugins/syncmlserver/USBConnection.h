23c0f94536e52eb29db32427ab18a67bbe2a0e94
Sateesh Kavuri <sateesh@mobilitas.co>
2013-07-12 09:41:15 +0000

    Initial structure of the server plugin
    To handle both bluetooth and USB connectors, USBConnection and BTConnection
    classes would be created. The USB connector would require a USB kernel module
    to create a /dev/ttyGS1 (possibly a gadget driver) that can handle the communication
    with the PC host. The PC host can be an application that talks SyncML and will
    act as a SyncML client, which this plugin will act as a OBEX server as
    well as SyncML server.
    
    Signed-off-by: Sateesh Kavuri <sateesh@mobilitas.co>

---
+++ serverplugins/syncmlserver/USBConnection.h
@@ -0,0 +1,63 @@
+/*
+* This file is part of buteo-sync-plugins package
+*
+* Copyright (C) 2013 Jolla Ltd. and/or its subsidiary(-ies).
+*
+* Author: Sateesh Kavuri <sateesh.kavuri@gmail.com>
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public License
+* version 2.1 as published by the Free Software Foundation.
+*
+* This library is distributed in the hope that it will be useful, but
+* WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+* 02110-1301 USA
+*/
+#ifndef USBCONNECTION_H
+#define USBCONNECTION_H
+
+#include <QString>
+
+#if QT_VERSION >= QT_VERSION_CHECK(5,0,0)
+#include <buteosyncml5/OBEXConnection.h>
+#else
+#include <buteosyncml/OBEXConnection.h>
+#endif
+
+/*! \brief Class for creating connection to a PC that acts as a USB
+ *         host for synchronization of data using buteosyncml
+ *
+ */
+class USBConnection : public DataSync::OBEXConnection
+{
+public:
+    USBConnection ();
+
+    virtual ~USBConnection ();
+
+    /*! \sa DataSync::OBEXConnection::connect ()
+     *
+     */
+    virtual int connect ();
+
+    /*! \sa DataSync::OBEXConnection::isConnected ()
+     *
+     */
+    virtual bool isConnected () const;
+
+    /*! \sa DataSync::OBEXConnection::disconnect ()
+     *
+     */
+    virtual void disconnect ();
+
+private:
+    int         mFd;
+};
+
+#endif // USBCONNECTION_H
---
+++ serverplugins/syncmlserver/syncmlserver.pro
@@ -0,0 +1,56 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2013-07-08T08:45:57
+#
+#-------------------------------------------------
+
+QT       -= gui
+
+TARGET = syncml-server
+TEMPLATE = lib
+
+CONFIG += link_pkgconfig plugin
+
+INCLUDEPATH += . ../../syncmlcommon
+LIBS += -L../../syncmlcommon
+
+equals(QT_MAJOR_VERSION, 4): {
+    PKGCONFIG = buteosyncfw buteosyncml
+    LIBS += -lsyncmlcommon
+    target.path = /usr/lib/buteo-plugins
+}
+
+equals(QT_MAJOR_VERSION, 5): {
+    PKGCONFIG = buteosyncfw5 buteosyncml5
+    LIBS += -lsyncmlcommon5
+    target.path = /usr/lib/buteo-plugins-qt5
+}
+
+VER_MAJ = 1
+VER_MIN = 0
+VER_PAT = 0
+
+QMAKE_CXXFLAGS = -Wall \
+    -g \
+    -Wno-cast-align \
+    -O2 -finline-functions
+
+DEFINES += SYNCMLSERVER_LIBRARY
+
+SOURCES += SyncMLServer.cpp \
+    USBConnection.cpp
+
+HEADERS += SyncMLServer.h\
+        syncmlserver_global.h \
+    USBConnection.h
+
+
+#cleanup
+QMAKE_CLEAN += $(TARGET) $(TARGET0) $(TARGET1) $(TARGET2)
+QMAKE_CLEAN += $(OBJECTS_DIR)/*.gcda $(OBJECTS_DIR)/*.gcno $(OBJECTS_DIR)/*.gcov $(OBJECTS_DIR)/moc_*
+
+sync.path = /etc/buteo/profiles/sync
+sync.files = xml/sync/*
+
+#installs
+INSTALLS += target sync
