39cda467dcedd1e92b19a0efc4c260d3c188dc81
Sateesh Kavuri <sateesh.kavuri@gmail.com>
2013-08-06 10:11:59 +0530

    Fixed issue with connecting signals of QSocketNotifiers from different threads
    QSocketNotifier belongs to thread ServerRunner while USBConnection
    belongs to the transport thread in buteo syncml stack. So it is not
    directly possible to connect signal/slots of these objects. The valid
    way is to use the 5'th argument of the QObject::connect to specify
    Qt::BlockingQueuedConnection instead of the default
    
    Signed-off-by: Sateesh Kavuri <sateesh.kavuri@gmail.com>

--- serverplugins/syncmlserver/USBConnection.cpp
+++ serverplugins/syncmlserver/USBConnection.cpp
@@ -245,20 +245,20 @@ USBConnection::addFdListener ()
         LOG_DEBUG ("Added fd listner for fd " << mFd << " with event source " << mFdWatchEventSource);
 #else
 
-    mReadNotifier = new QSocketNotifier (mFd, QSocketNotifier::Read, this);
-    mWriteNotifier = new QSocketNotifier (mFd, QSocketNotifier::Write, this);
-    mExceptionNotifier = new QSocketNotifier (mFd, QSocketNotifier::Exception, this);
+    mReadNotifier = new QSocketNotifier (mFd, QSocketNotifier::Read);
+    mWriteNotifier = new QSocketNotifier (mFd, QSocketNotifier::Write);
+    mExceptionNotifier = new QSocketNotifier (mFd, QSocketNotifier::Exception);
 
     mReadNotifier->setEnabled (true);
     mWriteNotifier->setEnabled (true);
     mExceptionNotifier->setEnabled (true);
 
     QObject::connect (mReadNotifier, SIGNAL (activated (int)),
-                      this, SLOT (handleUSBActivated (int)));
+                      this, SLOT (handleUSBActivated (int)), Qt::BlockingQueuedConnection);
     QObject::connect (mWriteNotifier, SIGNAL (activated (int)),
-                      this, SLOT (handleUSBActivated (int)));
+                      this, SLOT (handleUSBActivated (int)), Qt::BlockingQueuedConnection);
     QObject::connect (mExceptionNotifier, SIGNAL (activated (int)),
-                      this, SLOT (handleUSBError (int)));
+                      this, SLOT (handleUSBError (int)), Qt::BlockingQueuedConnection);
 #endif
     }
 }
