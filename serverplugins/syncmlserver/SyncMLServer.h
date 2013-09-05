d58ce85b0dd2f453bbf2a5774eeb98efb209431f
Sateesh Kavuri <sateesh.kavuri@gmail.com>
2013-09-05 17:21:09 +0530

    Bug fix for tracking the current active transport
    Earlier, while unloading the plugin, all the transports were closed
    irrespective of the current active transport. Now flags have been added
    to keep track of the current active transports and only those are closed
    
    Signed-off-by: Sateesh Kavuri <sateesh.kavuri@gmail.com>

--- serverplugins/syncmlserver/SyncMLServer.h
+++ serverplugins/syncmlserver/SyncMLServer.h
@@ -154,9 +154,25 @@ private:
 
     qint32                          mCommittedItems;
 
+    /**
+      * ! \brief The connectivity type used for the current sync session
+      */
     Sync::ConnectivityType          mConnectionType;
     
+    /**
+      * ! \brief Flag to indicate if the sync session is in progress
+      */
     bool                            mIsSessionInProgress;
+    
+    /**
+      * ! \brief Flag to indicate if bluetooth is active
+      */
+    bool                            mBTActive;
+    
+    /**
+      * ! \brief Flag to indicate if USB is active
+      */
+    bool                            mUSBActive;
 };
 
 #endif // SYNCMLSERVER_H
